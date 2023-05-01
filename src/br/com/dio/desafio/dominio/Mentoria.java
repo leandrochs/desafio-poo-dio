9dfb74bb72db5ff89a1569ea8f2a04647da8f0dd
leandro Silva <leandrochagask@gmail.com>
2023-05-01 16:59:31 -0300

    feat: adiciona get and set para novas variaveis

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -3,6 +3,28 @@ package br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 
 public class Mentoria extends Conteudo{
+    private String titulo;
+    private String descricao;
+
+    @Override
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
     private LocalDate data;
 

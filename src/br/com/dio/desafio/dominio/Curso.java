bcaf491fdea504fef51db7dc388bcc8865cb2106
leandro Silva <leandrochagask@gmail.com>
2023-05-01 16:57:30 -0300

    feat: adiciona get and set para novas variaveis

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,8 +1,29 @@
 package br.com.dio.desafio.dominio;
 
 public class Curso extends Conteudo{
-
+    private String titulo;
+    private String descricao;
     private int cargaHoraria;
+
+    @Override
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
     @Override
     public double calcularXp() {
